ffd0666b86e94f5f147fed1fe7be714b09e8a537
Miguel Azzopardi <miguelazz2000@gmail.com>
2022-08-22 20:11:44 +0200

    Table Reader Working
    Got a new page to read from a text file with the provided json data.
    
    Notes:
    Some errors with the provided json were noticed:
    1.Client 3's address had an invalid character '”' instead of '"'. That was fixed.
    
    2. Also some clients' variable names included a space, which was also fixed.
    
    Not sure if these were intentional errors, but nonetheless they were fixed.

---
+++ PaperLessBlazorTestProject/Classes/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace PaperLessBlazorTestProject.Classes
+{
+	public class Client
+	{
+		public int clientNo { get; set; }
+        public string clientName { get; set; }
+        public string countryCode { get; set; }
+        public string address { get; set; }
+	}
+}
